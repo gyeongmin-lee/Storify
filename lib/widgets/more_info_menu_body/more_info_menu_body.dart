1cfbcd8c9a3c8bacb49c4409835cd7343261574c
Gyeongmin Lee <metrostile@gmail.com>
2020-07-20 03:01:48 +0900

    configured playlist info menu

--- lib/widgets/more_info_menu_body/more_info_menu_body.dart
+++ lib/widgets/more_info_menu_body/more_info_menu_body.dart
@@ -1,14 +1,93 @@
 import 'package:flutter/material.dart';
 import 'package:storify/constants/style.dart';
+import 'package:storify/models/playlist.dart';
 import 'package:storify/models/user.dart';
+import 'package:storify/widgets/_common/custom_flat_text_button.dart';
 
 class MoreInfoMenuBody extends StatelessWidget {
+  final Playlist playlist = Playlist(
+      name: 'ROTATION',
+      creator: User(
+          name: 'METROSTILE',
+          avatarImageUrl:
+              'https://cdn.iconscout.com/icon/free/png-512/avatar-370-456322.png'),
+      playlistImageUrl:
+          'https://i.scdn.co/image/ab67616d0000b273ca086099c509810cb97fd227');
+
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Text(
-      "HELLO",
-      style: kBannerTextStyle,
+        child: Padding(
+      padding: const EdgeInsets.symmetric(vertical: 48.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          _buildPlaylistInfo(),
+          Column(
+            children: <Widget>[
+              CustomFlatTextButton(
+                text: 'FOLLOW ON SPOTIFY',
+                onPressed: () {},
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              CustomFlatTextButton(
+                text: 'SHARE AS LINK',
+                onPressed: () {},
+              ),
+              SizedBox(
+                height: 16.0,
+              ),
+              CustomFlatTextButton(
+                text: 'SHARE AS IG STORY',
+                onPressed: () {},
+              ),
+            ],
+          ),
+        ],
+      ),
     ));
+  }
+
+  Column _buildPlaylistInfo() {
+    return Column(
+      children: <Widget>[
+        CircleAvatar(
+            radius: 54.0,
+            backgroundImage: NetworkImage(playlist.playlistImageUrl)),
+        SizedBox(
+          height: 6.0,
+        ),
+        Text(
+          playlist.name,
+          style: kBannerTextStyle.copyWith(
+            letterSpacing: 2.0,
+          ),
+        ),
+        SizedBox(
+          height: 6.0,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Signed in as',
+                style: kBodyTextStyle.copyWith(fontSize: 14.0)),
+            SizedBox(
+              width: 8.0,
+            ),
+            CircleAvatar(
+                radius: 14.0,
+                backgroundImage: NetworkImage(playlist.creator.avatarImageUrl)),
+            SizedBox(
+              width: 8.0,
+            ),
+            Text(playlist.creator.name,
+                style: kAvatarTitleTextStyle.copyWith(fontSize: 16.0)),
+          ],
+        )
+      ],
+    );
   }
 }
