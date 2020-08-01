14513ffa5040e0dd38f7585e586b9f5bc85a682d
Gyeongmin Lee <metrostile@gmail.com>
2020-08-02 03:33:51 +0900

    cleared import

--- lib/widgets/player_page/player_page.dart
+++ lib/widgets/player_page/player_page.dart
@@ -1,6 +1,5 @@
 import 'dart:ui';
 
-import 'package:auto_size_text/auto_size_text.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
